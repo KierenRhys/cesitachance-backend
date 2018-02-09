878e5c0e567ed8a88410a52cb3f29c68605e58e0
Adrien <adrien2929@msn.com>
2018-02-09 10:07:21 +0100

    yolo

--- src/main/java/com/javahelps/restservice/controller/CandidateController.java
+++ src/main/java/com/javahelps/restservice/controller/CandidateController.java
@@ -3,6 +3,7 @@ package com.javahelps.restservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,21 +31,25 @@ public class CandidateController {
 	private ContactSchoolRepository repositoryContactSchool;
 
     @GetMapping
-    public Iterable<Candidate> findAll() {
+    @CrossOrigin
+    public List<Candidate> findAll() {
         return repository.findAll();
     }
 
+    @CrossOrigin
     @GetMapping(path = "/{candidate_id}")
     public Candidate find(@PathVariable("candidate_id") String candidate_id) {
         return repository.findOne(candidate_id);
     }
     
+    @CrossOrigin
     @GetMapping(path = "/{candidate_id}/interests")
     public String findInterests(@PathVariable("candidate_id") String candidate_id) {
     	String interests = repository.findOne(candidate_id).getInterests();
     	return interests;
     }
     
+    @CrossOrigin
 	@PostMapping(path= "/sync", consumes = "application/json")
     public Iterable<Candidate> create(@RequestBody List<Candidate> candidates) {
         for (Candidate candidate : candidates) {
@@ -53,6 +58,7 @@ public class CandidateController {
         return repository.findAll();
     }
 	
+    @CrossOrigin
 	@PostMapping(consumes = "application/json")
     public void createOne(@RequestBody Candidate candidate) {
     	repository.save(candidate);
@@ -64,11 +70,13 @@ public class CandidateController {
         }
     }
 
+    @CrossOrigin
     @DeleteMapping(path = "/{candidate_id}")
     public void delete(@PathVariable("candidate_id") String candidate_id) {
         repository.delete(candidate_id);
     }
 
+    @CrossOrigin
     @PutMapping(path = "/{candidate_id}")
     public Candidate update(@PathVariable("candidate_id") String candidate_id, @RequestBody Candidate candidate) throws BadHttpRequest {
         if (repository.exists(candidate_id)) {
